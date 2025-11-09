0105d089eb24f96176acd5bf3f013d95e60b3390
Rahul R Nayak <nayakrahul0852@gmail.com>
2025-11-09 18:56:33 +0530

    cicd 4

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -13,8 +13,8 @@ def promote_model():
     os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
     dagshub_url = "https://dagshub.com"
-    repo_owner = "vikashdas770"
-    repo_name = "YT-Capstone-Project"
+    repo_owner = "rahul-nayak01"
+    repo_name = "ML-OPs-Model-deployment-Mini-project-"
 
     # Set up MLflow tracking URI
     mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
