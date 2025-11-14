d84bcdddb56feceac57e7e7189f7142d4713c58d
Rahul R Nayak <nayakrahul0852@gmail.com>
2025-11-14 07:37:40 +0530

    model parm

--- src/model/model_building.py
+++ src/model/model_building.py
@@ -2,8 +2,26 @@ import numpy as np
 import pandas as pd
 import pickle
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
 import yaml
 from src.logger import logging
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logging.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logging.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logging.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logging.error('Unexpected error: %s', e)
+        raise
 
 
 def load_data(file_path: str) -> pd.DataFrame:
@@ -23,7 +41,10 @@ def load_data(file_path: str) -> pd.DataFrame:
 def train_model(X_train: np.ndarray, y_train: np.ndarray) -> MultinomialNB:
     """Train the Multinomial Naive Bayes model."""
     try:
-        clf = MultinomialNB()
+        params = load_params(params_path='params.yaml')
+        alp = params['model_building']['alpha']
+        clf = MultinomialNB(alpha=alp)
+        #clf = LogisticRegression(C=1, solver='liblinear', penalty='l2')
         clf.fit(X_train, y_train)
         logging.info('Model training completed')
         return clf
@@ -45,7 +66,7 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        
+
         train_data = load_data('./data/processed/train_bow.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
